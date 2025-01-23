00b3b6971e248e021ea3554ad65db334cf709375
Daniel Guinita <daniel.guinita@cit.edu>
2025-01-23 19:56:50 +0800

    commit again

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import styles from "./App.module.css";
+import { Navbar } from "./components/Navbar/Navbar";
 import { About } from "./components/About/About";
 import { Experience } from "./components/Experience/Experience";
 import { Hero } from "./components/Hero/Hero";
@@ -8,6 +9,7 @@ import { Footer } from "./components/Footer/Footer";
 function App() {
   return (
     <div className={styles.App}>
+      <Navbar/>
       <Hero />
       <About />
       <Experience />
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,27 +1,79 @@
 import React from "react";
 import styles from "./Hero.module.css";
 import { getImageUrl } from "../../utils";
+import Typewriter from "typewriter-effect";
 
 export const Hero = () => {
   return (
     <section className={styles.container}>
       <div className={styles.content}>
-        <h1 className={styles.title}>Hi!</h1>
+        <h3 className={styles.title}>Hi!</h3>
         <p className={styles.description}>
-          I'm Daniel Guinita, a 4th year Information Technology student, passionate about various fields in the tech industry.<br />
-          (Cybersecurity, IT Ops, Networking, QA Testing, Web Development)
+          <Typewriter
+            onInit={(typewriter) => {
+              setTimeout(() => {
+                typewriter
+                  .typeString(
+                    "I'm Daniel Guinita, a 4th year Information Technology student, passionate about various fields in the tech industry.<br/>(Cybersecurity, IT Ops, Networking, QA Testing, Web Development)"
+                  )
+                  .callFunction(() => {
+                    const buttonGroup = document.querySelector(
+                      `.${styles.buttonGroup}`
+                    );
+                    buttonGroup.style.opacity = 1;
+                    buttonGroup.classList.add(`${styles.visible}`);
+                  })
+                  .start();
+              }, 1000); // 1-second delay
+            }}
+            options={{
+              autoStart: true,
+              loop: false, // Prevents looping
+              delay: 25, // Adjust typing speed
+            }}
+          />
         </p>
         <div className={styles.buttonGroup}>
-          <a href="mailto:edu.dcguinita@gmail.com" className={styles.emailnBtn} target="_blank" rel="noopener noreferrer">
-            <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a
+            href="mailto:edu.dcguinita@gmail.com"
+            className={styles.emailBtn}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src={getImageUrl("contact/emailIcon.png")}
+              alt="Email icon"
+            />
           </a>
-          <a href="https://www.linkedin.com/in/dcguinita" className={styles.linkedinBtn} target="_blank" rel="noopener noreferrer">
-            <img src={getImageUrl("contact/linkedinIcon.png")} alt="LinkedIn icon" />
+          <a
+            href="https://www.linkedin.com/in/dcguinita"
+            className={styles.linkedinBtn}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src={getImageUrl("contact/linkedinIcon.png")}
+              alt="LinkedIn icon"
+            />
           </a>
-          <a href="https://www.github.com/daniel-guinita" className={styles.githubBtn} target="_blank" rel="noopener noreferrer">
-            <img src={getImageUrl("contact/githubIcon.png")} alt="GitHub icon" />
+          <a
+            href="https://www.github.com/daniel-guinita"
+            className={styles.githubBtn}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src={getImageUrl("contact/githubIcon.png")}
+              alt="GitHub icon"
+            />
           </a>
-          <a href="/GuinitaResume.pdf" className={styles.resumeBtn} download target="_blank" rel="noopener noreferrer">
+          <a
+            href="/GuinitaResume.pdf"
+            className={styles.resumeBtn}
+            download
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             Download Resume
           </a>
         </div>
@@ -29,6 +81,7 @@ export const Hero = () => {
       <img
         src={getImageUrl("hero/heroImage.png")}
         className={styles.heroImg}
+        alt="Hero"
       />
       <div className={styles.topBlur} />
       <div className={styles.bottomBlur} />
